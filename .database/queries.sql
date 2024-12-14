99ae935b4b5d1b047bdb9af8fae8703776572e8b
Siddhant Kaul <siddhant.kaul@education.nsw.gov.au>
2024-12-14 11:34:48 +1100

    initial commit for movie/game review app

---
+++ .database/queries.sql
@@ -0,0 +1,11 @@
+/*CREATE TABLE Guesses(id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    user_id INTEGER,
+                    date DATE NOT NULL,
+                    score INTEGER,
+                    game TEXT NOT NULL);
+
+CREATE TABLE Users(id INTEGER PRIMARy KEY AUTOINCREMENT,
+                    username TEXT UNIQUE NOT NULL,
+                    password NOT NULL);*/
+
+/*ALTER TABLE Guesses ADD COMMENTS TEXT;*/
